68e477b3722ca270d0c83462d843eee33974102f
Martin Siggel <martin.siggel@dlr.de>
2015-12-01 21:04:10 +0100

    Added FreeImage recipe

---
+++ freeimage/bld.bat
@@ -0,0 +1,25 @@
+mkdir buildd
+cd buildd
+
+REM Configure step
+cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+ -DCMAKE_BUILD_TYPE=Release ^
+ -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+ -DCMAKE_SYSTEM_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+ -DOCE_WIN_BUNDLE_INSTALL_DIR="%LIBRARY_PREFIX%" ^
+ -DBUNDLE_BUILD_FREEIMAGE=ON ^
+ -DBUNDLE_BUILD_FREETYPE=OFF ^
+ -DBUNDLE_BUILD_GL2PS=OFF ^
+ -DBUNDLE_SHARED_LIBRARIES=ON ^
+ -DOCE_MULTITHREAD_LIBRARY=OpenMP ^
+ ..
+if errorlevel 1 exit 1
+
+REM Build step 
+ninja
+if errorlevel 1 exit 1
+
+REM Install step
+ninja install
+if errorlevel 1 exit 1
+
