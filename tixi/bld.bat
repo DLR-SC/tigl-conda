085161da069e95f40e9dab8719038c98bdf47319
Martin Siggel <martin.siggel@dlr.de>
2015-12-03 23:22:26 +0100

    TiXI win64 package support

--- tixi/bld.bat
+++ tixi/bld.bat
@@ -1,6 +1,10 @@
 REM remove static linking to curl etc
+if %ARCH%==32 ( 
 python %RECIPE_DIR%\fixpaths.py lib\tixi\tixi-targets-release.cmake C:/Jenkins/Libs/lib-static-win32/libcurl_a.lib;C:/Jenkins/Libs/lib-static-win32/libxslt_a.lib;C:/Jenkins/Libs/lib-static-win32/libxml2_a.lib;
+) else (
+python %RECIPE_DIR%\fixpaths.py lib\tixi\tixi-targets-release.cmake C:/Jenkins/Libs/lib-static-win64/libcurl_a.lib;C:/Jenkins/Libs/lib-static-win64/libxslt_a.lib;C:/Jenkins/Libs/lib-static-win64/libxml2_a.lib;
 
+)
 
 copy bin\TIXI.dll %LIBRARY_PREFIX%\bin\
 copy bin\TIXI.dll %LIBRARY_PREFIX%\bin\
