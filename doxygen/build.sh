9b2470e47540c100f588aac13fd6e58a11b9a94c
Martin Siggel <martin.siggel@dlr.de>
2015-12-06 23:35:18 +0100

    Fixed build of doxygen on Mac

--- doxygen/build.sh
+++ doxygen/build.sh
@@ -1,14 +1,12 @@
 mkdir build
 cd build
 
-$PREFIX/bin/cmake \
-    -DCMAKE_INSTALL_PREFIX:PATH="$PREFIX" \
-    -DCMAKE_INSTALL_RPATH:STRING="$PREFIX" \
-    -DBUILD_SHARED_LIBS:BOOL=ON \
-    -Dbuild_wizard:BOOL=OFF \
+cmake \
+    -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
     ..
 
-make -j${CPU_COUNT}
+make -j ${CPU_COUNT}
 make install
 
-cp -v lib/* $PREFIX/lib/
