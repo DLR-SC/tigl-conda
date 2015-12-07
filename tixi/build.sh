cd39b2563f3e56db64dfaffe2cf0e612a3c56021
Martin Siggel <martin.siggel@dlr.de>
2015-12-07 16:20:27 +0100

    Forced absolute library path in tixiwrapper on linux

--- tixi/build.sh
+++ tixi/build.sh
@@ -18,4 +18,4 @@ make install
 mkdir $SP_DIR/tixi
 touch $SP_DIR/tixi/__init__.py
 cp lib/tixiwrapper.py $SP_DIR/tixi/
-python $RECIPE_DIR/fixosxload.py $SP_DIR/tixi/tixiwrapper.py libTIXI.dylib
+python $RECIPE_DIR/fixosxload.py $SP_DIR/tixi/tixiwrapper.py libTIXI
--- tixi/fixosxload.py
+++ tixi/fixosxload.py
@@ -1,26 +1,32 @@
 import fileinput, sys
 import os.path
 
-def replace(filein, intext, outtext):
-    f = open(filein,'r')
-    filedata = f.read()
-    f.close()
-
-
-    newdata = filedata.replace(intext, outtext)
-
-    f = open(filein,'w')
-    f.write(newdata)
-    f.close()
-
 if __name__ == "__main__":
     assert(len(sys.argv) == 3)
 
     filename = sys.argv[1]
     libname = sys.argv[2]
 
-    in_text = "lib = ctypes.CDLL(\"%s\")" % libname
-    out_text = "lib = ctypes.CDLL(os.path.join(sys.prefix, 'lib', '%s'))" % libname
+    f = open(filename,'r')
+    filedata = f.read()
+    f.close()
 
-    replace(filename, in_text, out_text)
-    replace(filename, "import sys, ctypes", "import sys, ctypes, os.path")
+    # fix mac paths
+    in_text = "lib = ctypes.CDLL(\"%s.dylib\")" % libname
+    out_text = "lib = ctypes.CDLL(os.path.join(sys.prefix, 'lib', '%s.dylib'))" % libname
+
+    filedata = filedata.replace(in_text, out_text)
+
+    # fix linux paths
+    in_text = "lib = ctypes.CDLL(\"%s.so\")" % libname
+    out_text = "lib = ctypes.CDLL(os.path.join(sys.prefix, 'lib', '%s.so'))" % libname
+
+    filedata = filedata.replace(in_text, out_text)
+
+
+    # fix imports
+    filedata = filedata.replace("import sys, ctypes", "import sys, ctypes, os.path")
+
+    f = open(filename,'w')
+    f.write(filedata)
+    f.close()
