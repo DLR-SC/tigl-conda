5c2e0519813f7d66ff587d6f8ba77c2bc4139414
Martin Siggel <martin.siggel@dlr.de>
2018-04-16 16:28:24 +0200

    Fix for memcpy glibc issue in libxslt

--- curl/build.sh
+++ curl/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-./configure --without-ssl --prefix=$PREFIX
+if [ `uname` == Darwin ]; then
+    ./configure --without-ssl --prefix=$PREFIX
+else
+    # link against rt, to lower glibc dependency to 2.14 (instead if 2.17)
+    LIBS=-lrt ./configure --without-ssl --prefix=$PREFIX
+fi
+
 make -j $CPU_COUNT
 make install
--- libxslt/build.sh
+++ libxslt/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export LDFLAGS="-lrt -Wl,--wrap=memcpy"
 ./configure --without-crypto --prefix=$PREFIX
 make -j $CPU_COUNT
 make install
