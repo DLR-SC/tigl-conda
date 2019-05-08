bd493862669679555b380b717cd427371cf0c47e
Martin Siggel <martin.siggel@dlr.de>
2019-05-09 00:11:33 +0200

    Remove linkage to dependencies in tixi package

--- tixi/bld.bat
+++ tixi/bld.bat
@@ -21,9 +21,3 @@ REM install python packages
 mkdir %SP_DIR%\tixi
 echo. 2> %SP_DIR%\tixi\__init__.py
 copy lib\tixiwrapper.py %SP_DIR%\tixi\
-
-REM remove static linking to curl etc
-python %RECIPE_DIR%\fixpaths.py %LIBRARY_PREFIX%\lib\tixi\tixi-targets-release.cmake %LIBRARY_PREFIX%/lib/libcurl_a.lib;
-python %RECIPE_DIR%\fixpaths.py %LIBRARY_PREFIX%\lib\tixi\tixi-targets-release.cmake %LIBRARY_PREFIX%/lib/libxslt_a.lib;
-python %RECIPE_DIR%\fixpaths.py %LIBRARY_PREFIX%\lib\tixi\tixi-targets-release.cmake %LIBRARY_PREFIX%/lib/libxml2_a.lib;
-
--- tixi/build.sh
+++ tixi/build.sh
@@ -1,23 +1,44 @@
+# To make libTIXI.so back-portable, we have to use clock_gettime
+# from librt instead of glibc. Thus we have to link rt explicitly
+
 mkdir build
 cd build
 
-export LDFLAGS="-Wl,--wrap=memcpy"
+if [ `uname` == Darwin ]; then
+    EXTRA_LIBS="-lm -liconv -framework Foundation -lz -framework Security $LDFLAGS"
+else
+    export LDFLAGS="-lrt -Wl,--wrap=memcpy"
+    EXTRA_LIBS="-lm -lrt $LDFLAGS"
+fi
+
+export CXXFLAGS="$CXXFLAGS -DGTEST_USE_OWN_TR1_TUPLE=1"
 
 # Configure step
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
- -DCMAKE_BUILD_TYPE=Release \
- -DCMAKE_PREFIX_PATH=$PREFIX \
- -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+ -DCMAKE_PREFIX_PATH=$CONDA_PREFIX \
+ -DCMAKE_SHARED_LINKER_FLAGS="$EXTRA_LIBS" \
+ -DCMAKE_EXE_LINKER_FLAGS="$EXTRA_LIBS" \
+ -DTIXI_BUILD_TESTS=ON \
  ..
 
 # Build step
 make -j $CPU_COUNT
 
+# remove linkage to static libs
+cmake .
+
 # Install step
 make install
 
+# Tests
+# make test
+
+# create the binary package
+#make package
+#cp *.tar.gz $RECIPE_DIR/
+
 # install python packages
-mkdir $SP_DIR/tixi
+mkdir -p $SP_DIR/tixi
 touch $SP_DIR/tixi/__init__.py
 cp lib/tixiwrapper.py $SP_DIR/tixi/
 python $RECIPE_DIR/fixosxload.py $SP_DIR/tixi/tixiwrapper.py libTIXI
