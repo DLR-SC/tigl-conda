3431cee3d0b37ce2bf9bbb27901ff86040cf051f
Martin Siggel <martin.siggel@dlr.de>
2019-10-22 22:22:57 +0200

    Simplify unix build of cmake

--- cmake/build.sh
+++ cmake/build.sh
@@ -13,17 +13,12 @@ export DEBUG_CXXFLAGS=$(echo "$DEBUG_CXXFLAGS" |sed -e "s|-I$PREFIX/include||")
 ./bootstrap \
              --verbose \
              --prefix="${PREFIX}" \
-             --system-libs \
-             --no-qt-gui \
-             --no-system-libarchive \
-             --no-system-jsoncpp \
              --parallel=${CPU_COUNT} \
              -- \
              -DCMAKE_BUILD_TYPE:STRING=Release \
              -DCMAKE_FIND_ROOT_PATH="${PREFIX}" \
              -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
              -DCURSES_INCLUDE_PATH="${PREFIX}/include" \
-             -DBUILD_CursesDialog=ON \
              -DCMake_HAVE_CXX_MAKE_UNIQUE:INTERNAL=FALSE \
              -DCMAKE_PREFIX_PATH="${PREFIX}" \
              -DCMAKE_CXX_STANDARD:STRING=17
