c28dc3c6d635d9b1cc7c6c3a93bc5f46846178e6
Martin Siggel <martin.siggel@dlr.de>
2020-01-01 01:19:37 +0100

    Rebuild tigl(2) package

--- tigl/build.sh
+++ tigl/build.sh
@@ -33,3 +33,15 @@ touch $SP_DIR/tigl/__init__.py
 cp lib/tiglwrapper.py $SP_DIR/tigl/
 mv $PREFIX/share/tigl/python/internal/* $SP_DIR/tigl/
 python $RECIPE_DIR/fixosxload.py $SP_DIR/tigl/tiglwrapper.py libTIGL
+
+# The egg-info file is necessary because some packages
+# might require tigl3 in their setup.py.
+# See https://setuptools.readthedocs.io/en/latest/pkg_resources.html#workingset-objects
+
+cat > $SP_DIR/tigl-$PKG_VERSION.egg-info <<FAKE_EGG
+Metadata-Version: 2.1
+Name: tigl
+Version: $PKG_VERSION
+Summary: The TiGL Geometry Library to process aircraft geometries in pre-design
+Platform: UNKNOWN
+FAKE_EGG
