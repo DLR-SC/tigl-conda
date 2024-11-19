31d42da946f5effb57c7bf1a7b782669399d440a
Jan Kleinert <jan.kleinert@dlr.de>
2024-11-19 14:35:02 +0100

    update cpacscreator package

--- cpacscreator/bld.bat
+++ cpacscreator/bld.bat
@@ -26,12 +26,23 @@ cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
 if errorlevel 1 exit 1
 
 REM Build step 
-cmake --build .
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
 REM Install step
-cmake --build . --target install
+cmake --build . --target install --config Release
 if errorlevel 1 exit 1
 
 REM install python packages
 move %LIBRARY_PREFIX%\share\tigl3\python\tigl3 %SP_DIR%
+
+REM The egg-info file is necessary because some packages,
+REM might require cpacscreator in their setup.py.
+REM See https://setuptools.readthedocs.io/en/latest/pkg_resources.html#workingset-objects
+
+set egg_info=%SP_DIR%\cpacscreator-%PKG_VERSION%.egg-info
+echo>%egg_info% Metadata-Version: 2.1
+echo>>%egg_info% Name: tigl3
+echo>>%egg_info% Version: %PKG_VERSION%
+echo>>%egg_info% Summary: The TiGL Geometry Library to process aircraft geometries in pre-design
+echo>>%egg_info% Platform: UNKNOWN
