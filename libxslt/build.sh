0daca6ac7037c4ce992b9626d35cc7a3a1806e32
Jan Kleinert <jan.kleinert@dlr.de>
2025-08-29 14:14:07 +0200

    fix linux build

--- libxslt/build.sh
+++ libxslt/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LDFLAGS="-lrt -Wl,--wrap=memcpy"
+export LDFLAGS="-lrt -Wl"
 ./configure --without-crypto --without-zlib -without-python --prefix=$PREFIX
 make -j $CPU_COUNT
 make install
