faa5ea7ce269651ecf6370e59855683b63ffc155
Jan Kleinert <jan.kleinert@dlr.de>
2025-09-01 16:50:28 +0200

    change order

--- libxslt-static/bld.bat
+++ libxslt-static/bld.bat
@@ -1,11 +1,11 @@
 cd win32
 
+cscript configure.js iconv=no prefix="%LIBRARY_PREFIX%" lib="%LIBRARY_PREFIX%"\lib include="%LIBRARY_PREFIX%"\include\libxml2
+if errorlevel 1 exit 1
+
 REM define LIBXML_STATIC to avoid linking against libxml2.dll
 set CFLAGS=/D LIBXML_STATIC
 set CXXFLAGS=/D LIBXML_STATIC
-
-cscript configure.js iconv=no prefix="%LIBRARY_PREFIX%" lib="%LIBRARY_PREFIX%"\lib include="%LIBRARY_PREFIX%"\include\libxml2
-if errorlevel 1 exit 1
 
 REM Build step 
 nmake
