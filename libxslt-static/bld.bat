f0a0af1f70703a77e765f0448eb441cdee3a0557
Jan Kleinert <jan.kleinert@dlr.de>
2025-09-02 12:25:50 +0200

    delete move of dll

--- libxslt-static/bld.bat
+++ libxslt-static/bld.bat
@@ -11,16 +11,11 @@ cmake .. ^
  -DLIBXSLT_WITH_PROFILER=OFF
 if errorlevel 1 exit 1
 
-
 REM Build step 
 cmake --build . --config Release --verbose
-
+if errorlevel 1 exit 1
 
 REM Install step
 cmake --install . --config Release --component Development
 cmake --install . --config Release --component Runtime
-if errorlevel 1 exit 1
-
-REM Remove test runners
-move "%LIBRARY_PREFIX%"\lib\lib*xslt.dll "%LIBRARY_PREFIX%"\bin\
 if errorlevel 1 exit 1
